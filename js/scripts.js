829a77ec3cb9947b2b3cb510d5f93803da826637
Chan Lee and Connor McCarthy <connor@>
2018-08-13 10:31:25 -0700

    refactor function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var answers = ["person1", 'person2', 'animal', 'exclamation', 'verb', 'noun']
+
+    answers.forEach(function(answer){
+      var userInput = $('input#' + answer).val();
+      $("." + answer).text(userInput);
+    });
 
     $("#story").show();
     event.preventDefault();
